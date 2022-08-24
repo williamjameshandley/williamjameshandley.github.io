05ce9e5d578eb46d0d6d80dd1da8246cfea0c696
Will Handley <wh260@cam.ac.uk>
2022-08-24 12:31:17 +0100

    Changed to grid rather than table

--- assets/students/html_table.py
+++ assets/students/html_table.py
@@ -9,10 +9,29 @@ df['level'] = df.data.apply(lambda x: min(x.levels).key)
 df['present'] =  df.data.apply(lambda x: x.end()==None) 
 
 html_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'students.html')
-#html_file = 'students.html'
+
+css = """
+.grid{
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-template-rows: 1fr;
+  column-gap: 20px;
+  row-gap: 20px;
+}
+
+@media screen and (max-width: 992px){
+   .grid{
+      grid-template-colums: repeat(2, 1fr);
+   }
+}
+"""
 
 with open(html_file, 'w') as f:
     doc = Doc()
+
+    with doc.tag('style'):
+        doc.asis(css)
+
     for i, df in enumerate([df[df.present], df[~df.present]]):
         for level in levels:
             sdf = df[df.level==level].data.to_list()
@@ -22,53 +41,52 @@ with open(html_file, 'w') as f:
                         doc.text('%ss' % levels[level].string)
                     else:
                         doc.text('Past %ss' % levels[level].string)
-                with doc.tag('table', style="border:none"):
+                with doc.tag('div', klass="grid"):
                     for student in sdf:
-                        with doc.tag('tr', style="border:none"):
-                            with doc.tag('td', style="width:30%;border:none"):
-                                if student.image:
-                                    src = os.path.join('/assets/students/', student.image)
-                                elif student.original_image:
-                                    src = os.path.join('/assets/students/', student.original_image)
-                                else:
-                                    src = "/assets/images/user.png"
-                                doc.stag('img', src=src, style="border-radius: 20%")
+                        with doc.tag('div', klass="grid-item"):
+                            if student.image:
+                                src = os.path.join('/assets/students/', student.image)
+                            elif student.original_image:
+                                src = os.path.join('/assets/students/', student.original_image)
+                            else:
+                                src = "/assets/images/user.png"
+                            doc.stag('img', src=src, style="border-radius: 20%")
 
-                            with doc.tag('td', style="border:none;"):
-                                name = student.name
-                                with doc.tag('p'):
-                                    with doc.tag('font', size="+2"):
-                                        doc.text(name)
-                                with doc.tag('p'):
-                                    with doc.tag('ul'):
-                                        for l in student.levels:
-                                            with doc.tag('li'):
-                                                doc.text('%s from %s' % (l, l.start.strftime("%b %Y")))
-                                                if l.end:
-                                                    doc.text(' to %s' % l.end.strftime("%b %Y"))
-                                                if l.supervisors:
-                                                    str(l.supervisors[0])
-                                                    with doc.tag('ul'):
-                                                        with doc.tag('li'):
-                                                            text = ', '.join([str(cs) for cs in  l.supervisors])
-                                                            doc.asis("co-supervised with %s" % text)
-    
-                                        joint_papers = student.joint_papers()
-                                        if joint_papers:
-                                            with doc.tag('li'):
-                                                doc.asis(joint_papers)
-
-                                        if student.links:
-                                            doc.line('li', 'Links:')
-                                            with doc.tag('ul'):
-                                                for l in student.links:
+                        with doc.tag('div', klass="grid-item"):
+                            name = student.name
+                            with doc.tag('p'):
+                                with doc.tag('font', size="+2"):
+                                    doc.text(name)
+                            with doc.tag('p'):
+                                with doc.tag('ul'):
+                                    for l in student.levels:
+                                        with doc.tag('li'):
+                                            doc.text('%s from %s' % (l, l.start.strftime("%b %Y")))
+                                            if l.end:
+                                                doc.text(' to %s' % l.end.strftime("%b %Y"))
+                                            if l.supervisors:
+                                                str(l.supervisors[0])
+                                                with doc.tag('ul'):
                                                     with doc.tag('li'):
-                                                        doc.asis(l)
+                                                        text = ', '.join([str(cs) for cs in  l.supervisors])
+                                                        doc.asis("co-supervised with %s" % text)
 
-                                        if student.destination:
-                                            doc.line('li', 'Subsequent career:')
-                                            with doc.tag('ul'):
-                                                for date, loc in student.destination.items():
-                                                    with doc.tag('li'):
-                                                        doc.asis("%s: %s" % (date.strftime("%b %Y"), loc))
+                                    joint_papers = student.joint_papers()
+                                    if joint_papers:
+                                        with doc.tag('li'):
+                                            doc.asis(joint_papers)
+
+                                    if student.links:
+                                        doc.line('li', 'Links:')
+                                        with doc.tag('ul'):
+                                            for l in student.links:
+                                                with doc.tag('li'):
+                                                    doc.asis(l)
+
+                                    if student.destination:
+                                        doc.line('li', 'Subsequent career:')
+                                        with doc.tag('ul'):
+                                            for date, loc in student.destination.items():
+                                                with doc.tag('li'):
+                                                    doc.asis("%s: %s" % (date.strftime("%b %Y"), loc))
     f.write(indent(doc.getvalue()))
