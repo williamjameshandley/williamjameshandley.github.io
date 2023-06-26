70b6e01e2af2c74a13719beb66961aba38c76de1
Will Handley <wh260@cam.ac.uk>
2023-06-26 16:55:48 +0100

    Corrected typo

--- assets/students/html_table.py
+++ assets/students/html_table.py
@@ -31,7 +31,7 @@ css = """
 }
 """
 
-ignore = ["George Carter", "Toby Lovick", "Stephen Pickman"]
+ignore = ["George Carter", "Toby Lovick", "Stephen Pickman", "Namu Kroupa"]
 
 with open(html_file, 'w') as f:
     doc = Doc()
